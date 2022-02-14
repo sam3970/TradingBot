05f3535435c0db499c295c8b2eb07d4b4bf822da
Your Name <you@example.com>
2022-02-14 15:20:30 +0900

    v0.7.4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### TradingBot
 - Name       : TradingBot 
-- Ver        : 0.7.3
+- Ver        : 0.7.4
 - Creator    : sam3970
 - Descriptor : Coin Price && Information Alert Bot
--- Util.js
+++ Util.js
@@ -2,7 +2,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
 
     if (msg == "/한강" || msg == "/한강물" || msg == "/한강물온도" || msg == "/한강물 온도" || msg == "/한강 온도" || msg == "/한강온도")
     {
-        var temperuture = Utils.getHtmlFromWeb("https://api.hangang.msub.kr/")
+        let temperuture = Utils.getHtmlFromWeb("https://api.hangang.msub.kr/")
 
         temperuture = temperuture.split(",")[2]
         temperuture = temperuture.split(":")[1]
@@ -15,9 +15,9 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
     
     if (msg.includes("/코로나") || msg.includes("/우한폐렴") || msg.includes("/covid") || msg.includes("/COVID"))
     {
-        var covid_confirmation = Utils.getHtmlFromWeb("https://capi.msub.kr/")
-        var covid_dead = Utils.getHtmlFromWeb("https://capi.msub.kr/")
-        var covid_time = Utils.getHtmlFromWeb("https://capi.msub.kr/")
+        let covid_confirmation = Utils.getHtmlFromWeb("https://capi.msub.kr/")
+        let covid_dead = Utils.getHtmlFromWeb("https://capi.msub.kr/")
+        let covid_time = Utils.getHtmlFromWeb("https://capi.msub.kr/")
         
         covid_time = covid_time.split(",")[2]
         covid_time = covid_time.split(":")[1]
@@ -35,6 +35,38 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
         covid_confirmation = covid_confirmation.replace(/,/g,"")
         covid_dead = covid_dead.replace(/\"/gi, "")
 
-        replier.reply("금일 "+covid_time+" 확진자는 "+covid_confirmation+"명 사망자는 "+covid_dead+"명 입니다.\n-by msub");
+        replier.reply("금일 "+covid_time+" 확진자는 "+covid_confirmation+"명 사망자는 "+covid_dead+"명 입니다.\n-by 질병관리청");
+    }
+
+    if (msg.includes("/탐욕") || msg.includes("/공포"))
+    {
+        let fear = Utils.getHtmlFromWeb("https://api.btctools.io/api/fear-greed")
+        
+        let fear_value = fear.split(":")[3].split(",")[0]
+
+        let result_Disply
+        
+        if (fear_value > 0 && fear_value < 25)
+        {
+          result_Display = "극한 공포"          
+        }
+        else if (fear_value >= 25 && fear_value < 50)
+        {
+          result_Display = "공포"          
+        }
+        else if (fear_value >= 50 && fear_value < 75)
+        {
+          result_Display = "탐욕"          
+        }
+        else if (fear_value >= 75 && fear_value <= 100)
+        {
+          result_Display = "극단적 탐욕"          
+        }
+        else
+        {
+          result_Display = "잘못된 값이 출력되었습니다. 관리자에게 문의바랍니다."          
+        }
+
+        replier.reply("탐욕 수치 : "+fear_value+"\n탐욕 지수 : "+result_Display+"\n-by btctools");
     }
 }
